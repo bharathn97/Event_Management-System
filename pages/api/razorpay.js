c22603b4331a6f4941a67e5f6fa86ad27ee633a3
bharathn97 <118900692+bharathn97@users.noreply.github.com>
2024-07-07 17:34:59 +0530

    Create razorpay.js

---
+++ pages/api/razorpay.js
@@ -0,0 +1,37 @@
+const Razorpay = require("razorpay");
+const shortid = require("shortid");
+
+export default async function handler(req, res) {
+  const { taxAmt } = req.body;
+  //console.log('taxAmt',taxAmt*100)
+  if (req.method === "POST") {
+    // Initialize razorpay object
+    const razorpay = new Razorpay({
+      key_id: "rzp_test_nVaSW2cxt1Jb0m",
+      key_secret: "k3ib6MNVkP7huNb3GlM0ZZzO",
+    });
+    const payment_capture = 1;
+    const amount = taxAmt;
+    const currency = "INR";
+    const options = {
+      amount: (amount * 100).toString(),
+      currency,
+      receipt: shortid.generate(),
+      payment_capture,
+    };
+
+    try {
+      const response = await razorpay.orders.create(options);
+      res.status(200).json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount,
+      });
+    } catch (err) {
+      //console.log(err);
+      res.status(400).json(err);
+    }
+  } else {
+    // Handle any other HTTP method
+  }
+}
