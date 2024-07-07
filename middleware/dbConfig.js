f6498865df2428f107a960efd62859397824d299
bharathn97 <118900692+bharathn97@users.noreply.github.com>
2024-07-07 17:35:55 +0530

    Update dbConfig.js

--- middleware/dbConfig.js
+++ middleware/dbConfig.js
@@ -1,13 +1,17 @@
-// dbConfig.js
+const fs = require('fs');
+const path = require('path');
+
 const dbConfig = {
-  user: "root",
-  password: "Bh@rath9",
-  server: "localhost",
-  port: 3306,
-  database: "dbms",
+  user: "avnadmin", // Aiven MySQL username
+  password: "AVNS_D8ttYpaFCLdfa5QZri8", // Replace with your Aiven MySQL password
+  server: "mysql-4b737c1-bharathnagendrababu-2de0.h.aivencloud.com", // Aiven MySQL hostname
+  port: 12272, // Aiven MySQL port
+  database: "defaultdb", 
   options: {
-    encrypt: false, // If you are using Azure, set to true
-    trustServerCertificate: true, // For local development, you might need to set this to true
+    encrypt: true, 
+    ssl: {
+      ca: fs.readFileSync(path.join(__dirname, '../ca.pem')) 
+    },
   },
 };
 
