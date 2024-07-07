8117ff56970583db6636ed794822368d4037a1a5
bharathn97 <118900692+bharathn97@users.noreply.github.com>
2024-07-07 18:03:33 +0530

    Update razorpay.js

--- pages/api/razorpay.js
+++ pages/api/razorpay.js
@@ -1,30 +1,37 @@
-{
-  "name": "dbms",
-  "version": "0.1.0",
-  "private": true,
-  "scripts": {
-    "dev": "next dev -p 3002",
-    "build": "next build",
-    "start": "next start",
-    "lint": "next lint"
-  },
-  "dependencies": {
-    "@prisma/client": "^5.10.2",
-    "bcrypt": "^5.1.1",
-    "dotenv": "^16.4.5",
-    "mysql": "^2.18.1",
-    "mysql2": "^3.9.2",
-    "next": "14.0.4",
-    "prisma": "^5.10.2",
-    "razorpay": "^2.8.5",
-    "react": "^18",
-    "react-big-calendar": "^1.11.2",
-    "react-dom": "^18",
-    "react-toastify": "^10.0.4"
-  },
-  "devDependencies": {
-    "autoprefixer": "^10.0.1",
-    "postcss": "^8",
-    "tailwindcss": "^3.3.0"
+const Razorpay = require("razorpay");
+const shortid = require("shortid");
+
+export default async function handler(req, res) {
+  const { taxAmt } = req.body;
+  //console.log('taxAmt',taxAmt*100)
+  if (req.method === "POST") {
+    // Initialize razorpay object
+    const razorpay = new Razorpay({
+      key_id: "rzp_test_nVaSW2cxt1Jb0m",
+      key_secret: "k3ib6MNVkP7huNb3GlM0ZZzO",
+    });
+    const payment_capture = 1;
+    const amount = taxAmt;
+    const currency = "INR";
+    const options = {
+      amount: (amount * 100).toString(),
+      currency,
+      receipt: shortid.generate(),
+      payment_capture,
+    };
+
+    try {
+      const response = await razorpay.orders.create(options);
+      res.status(200).json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount,
+      });
+    } catch (err) {
+      //console.log(err);
+      res.status(400).json(err);
+    }
+  } else {
+    // Handle any other HTTP method
   }
 }
