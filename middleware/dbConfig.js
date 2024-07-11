6b3a90d940625dd1b09e38b80ed90eaf936f3252
bharathn97 <118900692+bharathn97@users.noreply.github.com>
2024-07-11 19:47:20 +0530

    Update dbConfig.js

--- middleware/dbConfig.js
+++ middleware/dbConfig.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const dbConfig = {
-  user: "avnadmin", // Aiven MySQL username
-  password: "AVNS_D8ttYpaFCLdfa5QZri8", // Replace with your Aiven MySQL password
-  server: "mysql-4b737c1-bharathnagendrababu-2de0.h.aivencloud.com", // Aiven MySQL hostname
-  port: 12272, // Aiven MySQL port
-  database: "defaultdb", 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   options: {
     encrypt: true, 
     ssl: {
